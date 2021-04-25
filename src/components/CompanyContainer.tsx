5956a0fd0543389852d7dd3bb0ecb8fd5d9c5bed
lvuletic <luka.vuletic1999@gmail.com>
2021-04-25 15:16:23 +0200

    render day

--- src/components/CompanyContainer.tsx
+++ src/components/CompanyContainer.tsx
@@ -4,6 +4,7 @@ import ITime_slot from '../interfaces/ITime_slot';
 import ICompanyReservation from '../interfaces/ICompanyReservation';
 import { CompanyName } from './CompanyName';
 import { TimeSlot } from './TimeSlot';
+import moment from 'moment';
 
 interface Props {
     company: ICompany;
@@ -28,15 +29,26 @@ export const CompanyContainer: React.FC<Props> = ({ company, companyReservation,
                 </span>
             }
             {company.time_slots.map((timeSlot: ITime_slot, idx: number) => {
+                const dayOfCurrentSlot: number = company.time_slots[idx - 1] ? moment(company.time_slots[idx - 1].start_time).dayOfYear() : moment(company.time_slots[idx].start_time).dayOfYear();
+                const dayOfOncomingSlot: number = moment(company.time_slots[idx].start_time).dayOfYear();
+                let shouldRenderDay: boolean = false;
+                if ((dayOfCurrentSlot !== dayOfOncomingSlot) || (idx === 0)) {
+                    shouldRenderDay = true;
+                }
+                const dayToRender: string = moment(company.time_slots[idx].start_time).format('dddd');
                 return (
-                    <div
-                        key={idx}
-                        className={`timeslot ${(timeSlot.isTakenUnderOtherCompany && ' timeslot-disabled')}`}
-                        onClick={() => selectTimeSlot && !timeSlot.isTakenUnderOtherCompany && selectTimeSlot(company, timeSlot)}
-                    >
-                        <TimeSlot
-                            timeSlot={timeSlot}
-                        />
+                    <div key={idx}>
+                        {shouldRenderDay &&
+                            <h1>{dayToRender}</h1>
+                        }
+                        <div
+                            className={`timeslot ${(timeSlot.isTakenUnderOtherCompany && ' timeslot-disabled')}`}
+                            onClick={() => selectTimeSlot && !timeSlot.isTakenUnderOtherCompany && selectTimeSlot(company, timeSlot)}
+                        >
+                            <TimeSlot
+                                timeSlot={timeSlot}
+                            />
+                        </div>
                     </div>
                 )
             })}
